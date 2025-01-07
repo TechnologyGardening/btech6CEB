47e8e7279c5e639606b5312d563aa0617547b0c1
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2025-01-07 13:43:44 +0530

    class exercise

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import MyCustButton from "./lec3112/purefunc2";
-import HookExmp1 from "./lec3112/hookexmp1";
+import ClassExercise from "./LEC0701/exercise1";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <MyCustButton />
-    <br />
-    <MyCustButton var1={true} />
-    <br />
-    <MyCustButton var2={"6CEB"} />
-    <br />
-    <MyCustButton var1={true} var2={"BTech"} /> */}
-    <HookExmp1 />
+    <ClassExercise />
   </StrictMode>
 );
